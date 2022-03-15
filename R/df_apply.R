1ff46fe6f1734ca71e0f69b277f5ebb91c3ae95a
smk92 <smk92@rstudio.cs.calvin.edu>
2022-03-15 13:17:46 -0400

    added license and roxygen header

--- R/df_apply.R
+++ R/df_apply.R
@@ -1,3 +1,24 @@
+#' Apply a function to a data frame
+#'
+#' @param .data A data frame
+#' @param .f A function you would like to apply to a column(s) of the data frame
+#' @param .condition A condition that column must have to apply the function
+#' @param .else If the condition fails, a second function to apply
+#' @param ... Allows for arguments to be passed into .f
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' n <- 7L
+#' TestData <-
+#'  tibble( double = rnorm(n, 100, 10),
+#'          x = 123400 / 10^(1L:n),
+#'          integer = (1L:n) * (1L:n),    # ^2 would return a double!!
+#'          character = LETTERS[1L:n],
+#'          factor = factor(letters[1L:n]),
+#'          logical = rep(c(TRUE, FALSE), length.out = n) )
+#' df_apply(TestData, round, is.numeric, toupper, digits = 1)
 df_apply <- function(.data, .f, .condition=function(x) {TRUE}, .else=function(x) {x}, ...) {
   .data |>
     lapply(
