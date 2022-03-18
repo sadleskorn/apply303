c65e0d8cf25e4216d78a4833f41e913a15b2dff8
dak23 <dak23@rstudio.cs.calvin.edu>
2022-03-18 00:49:49 -0400

    changed data for ufos and edited raw-data script

--- data-raw/ufo_raw_processed.R
+++ data-raw/ufo_raw_processed.R
@@ -1,16 +1,22 @@
 
-### Inspect Data
-ufos_raw <- read_csv("ufo_raw.csv") %>%
+library(tidyverse)
+library(plotly)
+library(dplyr)
+library(lubridate)
+library(fastR2)
+library(knitr)
+
+
+
+ufos_raw <- read_csv("ufos-scrubbed.csv") %>%
   filter(country == "us")
-
-ufos_raw <- utils::read.csv("data-raw/ufos-scrubbed.csv") %>%
-  dplyr::filter(country == "us")
-
+head(ufos_raw) %>% kable("html")
 
 #Data transformation
 ufos_raw$comments <- str_wrap(ufos_raw$comments, 50)
 ufos_raw$year <-format(as.Date(ufos_raw$datetime,format="%m/%d/%Y"),"%Y")
 ufos_raw$month <-format(as.Date(ufos_raw$datetime,format="%m/%d/%Y"),"%Y/%m")
+
 
 ### Filter down data to just the columns we need for visualization.
 #Select just the data needed
@@ -19,5 +25,3 @@ ufos <- ufos_raw %>%
   select(c(datetime, latitude, longitude, month, comments, state, city, year, shape))
 
 usethis::use_data(ufos, overwrite = TRUE)
-
-
