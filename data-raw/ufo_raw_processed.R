a80227283e0bde11ba6b6564316f7b4704b6613d
dak23 <dak23@rstudio.cs.calvin.edu>
2022-03-18 15:47:02 -0400

    changed pipe to new pipe

--- data-raw/ufo_raw_processed.R
+++ data-raw/ufo_raw_processed.R
@@ -5,15 +5,15 @@ library(lubridate)
 library(fastR2)
 library(knitr)
 
-ufos_raw <- read_csv("data-raw/ufos-scrubbed.csv") %>%
+ufos_raw <- read_csv("data-raw/ufos-scrubbed.csv") |>
   filter(country == "us")
 
 ufos_raw$comments <- str_wrap(ufos_raw$comments, 50)
 ufos_raw$year <-format(as.Date(ufos_raw$datetime,format="%m/%d/%Y"),"%Y")
 ufos_raw$month <-format(as.Date(ufos_raw$datetime,format="%m/%d/%Y"),"%Y/%m")
 
-ufos <- ufos_raw %>%
-  filter(year >= '2000') %>%
+ufos <- ufos_raw |>
+  filter(year >= '2000') |>
   select(c(datetime, latitude, longitude, month, comments, state, city, year, shape))
 
 usethis::use_data(ufos, overwrite = TRUE)
