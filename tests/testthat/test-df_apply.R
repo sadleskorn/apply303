ed3631e5a0bf6802bbe8e700d66684296cc5fb62
ehl6 <ehl6@rstudio.cs.calvin.edu>
2022-03-20 14:52:31 -0400

    Added more unit tests

--- tests/testthat/test-df_apply.R
+++ tests/testthat/test-df_apply.R
@@ -26,8 +26,20 @@ test_that("df_apply works", {
     int = c(1, 2, 3, 4, 5)
   )
 
+  # Apply to just a few columns of test df
+  TestRes_3 <- tibble::tibble(
+    double = c(16.96)
+  )
+
+  TestRes_4 <- tibble::tibble(
+    l_char = c('z', 'y', 'x', 'w', ''),
+    factor = factor(c('red', 'orange', 'yellow', 'green', 'blue')),
+    int = c(3, 3, 3, 3, 3)
+  )
+
   expect_equal(df_apply(TestData, round, is.numeric), TestRes_1)
   expect_equal(df_apply(TestData, round, is.numeric, toupper, digits = 1), TestRes_2)
   # ^ So this makes the factor column into a character column... are we worried about that?
-
+  expect_equal(df_apply(TestData[2], sum, is.numeric), TestRes_3)
+  expect_equal(df_apply(TestData[3:5], mean, is.numeric), TestRes_4)
 })
